37eb2e8a962b7cf88205783da5ff924395ccd7f1
hennickc <hennickc@amazon.com>
2025-03-05 20:23:30 -0800

    Bug fix

--- scripts/chess-challenge/Bots/Bot_1337.cs
+++ scripts/chess-challenge/Bots/Bot_1337.cs
@@ -126,7 +126,7 @@ public class Bot_1337 : IChessBot {
                                                     ENEMY_PIECE_VALUE_MULTIPLIER);
                                                 long bestResponseToResponseToResponse;
                                                 if (responseScoreZobrist.TryGetValue(board.ZobristKey, out bestResponseToResponseToResponse)) {
-                                                    responseToResponseScore += minOpptMovesScore(boardStateAfterMove.legalMoves) - bestResponseToResponseToResponse - responseToResponseMinOpptMovesBaseline;
+                                                    responseToResponseScore += minOpptMovesScore(boardStateAfterResponseToResponse) bestResponseToResponseToResponse - responseToResponseMinOpptMovesBaseline;
                                                 }
                                             }
                                         }
