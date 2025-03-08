2169e0ebd4c7000176f924843044816eb9143ac9
hennickc <hennickc@amazon.com>
2025-03-07 19:57:53 -0800

    Tweak: set  GC threshold at exact power of 2 rather than power of 2 plus 1

--- scripts/chess-challenge/Bots/Bot_1337.cs
+++ scripts/chess-challenge/Bots/Bot_1337.cs
@@ -106,13 +106,13 @@ public class Bot_1337 : IChessBot {
                 alphaBetaCache[i] = null;
             }
             currentMonotonicKey = newCurrentMonotonicKey;
-            if (trimmedCacheEntries > 1 << 26) {
+            if (trimmedCacheEntries >= 1 << 26) {
                 // In alphaBetaCache: 
                 // - 4 bytes for object header
                 // - 8 bytes for ulong key
                 // - ~20 bytes for value since it's Pack(8)
                 // -> 32 (1 << 5) bytes total
-                // so each GC should free ~1GiB (1 << 3` bytes)
+                // so each GC should free ~2GiB (1 << 31 bytes)
                 GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive, false, true);
                 trimmedCacheEntries = 0;
             }
